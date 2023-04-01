202a25caa11102ed8fe00cf3c90085f6d60cb4be
ehgur062300 <79880998+ehgur062300@users.noreply.github.com>
2023-04-01 20:32:06 +0900

    [level 2] Title: 위장, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.cpp
+++ 프로그래머스/lv2/42578. 위장/위장.cpp
@@ -6,16 +6,14 @@ using namespace std;
 
 int solution(vector<vector<string>> clothes) {
     int answer = 1;
- 
-    map<string, int> m;
-    map<string, int>::iterator it;
+    map<string,int> m;
+    map<string,int>::iterator iter;
     
     for(int i=0; i<clothes.size(); i++){
-        m[clothes[i][1]]++;
+        m[clothes[i][1]]++;    
     }
-    
-    for(it=m.begin(); it != m.end(); it++){
-        answer *= it->second+1;
+    for(iter = m.begin(); iter != m.end(); iter++){
+        answer *= iter->second+1;
     }
     
     return answer-1;
