a9a30115057d9a352b097aeb6737eeaa6153d484
ehgur062300 <79880998+ehgur062300@users.noreply.github.com>
2023-04-08 16:28:36 +0900

    [level 2] Title: 타겟 넘버, Time: 0.55 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -2,15 +2,16 @@
 #include <vector>
 
 using namespace std;
+
 int answer = 0;
 
-void DFS(vector<int> numbers, int target, int sum, int cnt){
+void DFS(vector<int>numbers, int target, int sum, int cnt){
     int level = numbers.size();
+    
     if(cnt == level){
         if(sum == target){ answer++; }
         return;
     }
-    
     DFS(numbers, target, sum+numbers[cnt], cnt+1);
     DFS(numbers, target, sum-numbers[cnt], cnt+1);
 }
