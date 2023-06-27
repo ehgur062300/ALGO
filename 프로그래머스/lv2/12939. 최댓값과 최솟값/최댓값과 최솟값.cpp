8def6cf65275209339189922e1ef61079355c46b
ehgur062300 <79880998+ehgur062300@users.noreply.github.com>
2023-06-27 14:44:55 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -5,34 +5,20 @@
 
 using namespace std;
 
-
-vector<string> split(string s, char dlim){
-    vector<string> str;
+string solution(string s) {
+    string answer = "";
+    vector<int> str;
     string sbuffer;
     stringstream ss;
 
     ss.str(s);
 
-    while(getline(ss,sbuffer, dlim)){
-        str.push_back(sbuffer);
+    while(getline(ss, sbuffer, ' ')){
+        str.push_back(stoi(sbuffer));
     }
-    return str;
-}
 
-bool cmp(string a, string b)
-{
-    return stoi(a) < stoi(b); // 1 2 3 4 5 6 7 8 9 10   뒤에있는 숫자가 더 크다.
-}
-
-string solution(string s) {
-    string answer = "";
-
-    vector<string> re = split(s,' ');
-
-    sort(re.begin(), re.end(), cmp);
-    answer.append(*re.begin());
-    answer.append(" ");
-    answer.append(re.back());
+    sort(str.begin(), str.end());
+    answer = to_string(str.front()) + " " + to_string(str.back());
 
     return answer;
 }
