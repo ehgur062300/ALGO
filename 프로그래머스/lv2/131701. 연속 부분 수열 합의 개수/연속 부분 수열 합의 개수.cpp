e891772e6d77d563e609baa3b2ab389bdd5474e4
ehgur062300 <79880998+ehgur062300@users.noreply.github.com>
2023-06-29 14:47:59 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 609.74 ms, Memory: 22.7 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -1,18 +1,18 @@
 #include <string>
 #include <vector>
-#include <map>
+#include <set>
 
 using namespace std;
 
 int solution(vector<int> elements) {
-    map<int,int> sums;
+    set<int> sums;
 
     int len = elements.size();
     for (int i = 0; i < len; i++) {
         int sum = 0;
         for (int j = 0; j < len; j++) {
             sum += elements[(i+j) % len];
-            sums.insert({sum,1});
+            sums.insert(sum);
         }
     }
     return sums.size();
