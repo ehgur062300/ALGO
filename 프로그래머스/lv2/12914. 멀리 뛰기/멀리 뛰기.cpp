a1fde1c0886bea99b57cac29b33f7b748a39b317
ehgur062300 <79880998+ehgur062300@users.noreply.github.com>
2023-07-05 13:29:39 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.cpp
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -4,13 +4,11 @@
 using namespace std;
 
 long long solution(int n) {
-    long long answer = 0;
-    long long dp[2001]={};
+    long long dp[2001];
     dp[0] = 1;
     dp[1] = 1;
     for(int i=2; i<=n; i++){
-        dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+        dp[i] = (dp[i-1]+dp[i-2])%1234567;
     }
-    answer = dp[n];
-    return answer;
+    return dp[n];
 }
