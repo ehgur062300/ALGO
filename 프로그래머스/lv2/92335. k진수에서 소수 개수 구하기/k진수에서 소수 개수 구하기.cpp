28d71bc1edb2ee87da16d24d120cfe26e03c90b8
ehgur062300 <79880998+ehgur062300@users.noreply.github.com>
2023-07-28 14:33:58 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.01 ms, Memory: 4.05 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.cpp
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.cpp
@@ -6,8 +6,8 @@
 using namespace std;
 
 bool isPrime(long long n) {
-    if(n == 1){ return false; }
-	for (int i = 2; i <= sqrt(n); i++) {
+    if(n < 2){ return false; }
+	for (long long i = 2; i <= sqrt(n); i++) {
 		if (n%i == 0) { return false; }
 	}
 	return true;
@@ -25,21 +25,21 @@ int solution(int n, int k) {
     
     if(v.size() == 1){
         if(isPrime(v[0])){ return 1; }
+        return 0;
     }
     
-    string temp = "";
-    if(v[0] != 0){ temp += to_string(v[0]); }
-    for(int i=1; i<v.size(); i++){
-        if(v[i] == 0 && v[i-1] == 0){ continue; }
-        else if(v[i] == 0 && v[i-1] != 0){
-            if(isPrime(stol(temp))){ answer++; }
-            temp = "";
-            continue;
+    string temp = "0";
+    for(int i=0; i<v.size(); i++){
+        if(v[i] == 0){
+            if(isPrime(stoll(temp))){ answer++; }
+            temp = "0";
         }
-        temp += to_string(v[i]);
-        
-        if(i+1 == v.size()){
-            if(isPrime(stol(temp))){ answer++; }
+        else {
+            temp += to_string(v[i]);
+
+            if(i+1 == v.size()){
+                if(isPrime(stoll(temp))){ answer++; }
+            }
         }
     }
     return answer;
