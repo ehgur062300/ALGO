10a3033ea49794a57d7e7f81fea3e9b27fafd3c9
ehgur062300 <79880998+ehgur062300@users.noreply.github.com>
2023-09-14 16:01:39 +0900

    [level 1] Title: 행렬의 덧셈, Time: 12.32 ms, Memory: 116 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr1[0].length];
+        
+        for(int i=0; i<arr1[0].length; i++){
+            answer[0][i] = arr1[0][i] + arr2[0][i];
+            
+            for(int j=0; j<arr1.length; j++){
+                answer[j][i] = arr1[j][i] + arr2[j][i];    
+            }
+        }
+        return answer;
+    }
+}
