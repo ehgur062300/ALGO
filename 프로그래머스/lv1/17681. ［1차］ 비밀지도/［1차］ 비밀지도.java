3b6edbc021d58f2377a71b73223dca6fbf5b62f4
ehgur062300 <79880998+ehgur062300@users.noreply.github.com>
2023-09-23 20:43:29 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.38 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        String s = "";
+        
+        for(int i=0; i<n; i++){
+            s = Integer.toBinaryString((arr1[i] | arr2[i]));
+            s = s.replaceAll("0"," ");
+            s = s.replaceAll("1","#");
+            
+            answer[i] = s;
+            while (answer[i].length() < n) {
+                answer[i] = ' ' + answer[i];
+            }
+        }
+        return answer;
+    }
+}
