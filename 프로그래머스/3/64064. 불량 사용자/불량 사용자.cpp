d4856368b83d7f4a9b2362494ba82237fd44ab4a
ehgur062300 <79880998+ehgur062300@users.noreply.github.com>
2023-10-16 09:12:25 +0900

    [level 3] Title: 불량 사용자, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.cpp
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.cpp
@@ -3,6 +3,7 @@
 #include <set>
 
 using namespace std;
+
 set<int> s;
 int len;
 bool check[8];
@@ -15,23 +16,19 @@ void dfs(vector<string>& u_id, vector<string>& b_id, int i, int num){
     
     for(int j = 0; j < u_id.size(); j++){
         int z;
-        if(u_id[j].size() != b_id[i].size())
-            continue;
-        
-        if(check[j] == true)
-            continue;
-        
-        for(z = 0; z < u_id[j].size(); z++){
-            if(b_id[i][z] == '*')
-                continue;
-            if(u_id[j][z] != b_id[i][z])
-                break;
-        }
-        
-        if(z == u_id[j].size()){ 
-            check[j] = true;
-            dfs(u_id, b_id, i + 1, num | (1 << j));
-            check[j] = false;     
+        if(u_id[j].size() == b_id[i].size() && check[j] == false){
+            for(z = 0; z < u_id[j].size(); z++){
+                if(b_id[i][z] == '*')
+                    continue;
+                if(u_id[j][z] != b_id[i][z])
+                    break;
+            }
+
+            if(z == u_id[j].size()){ 
+                check[j] = true;
+                dfs(u_id, b_id, i + 1, num | (1 << j));
+                check[j] = false;     
+            }
         }
     }
 
