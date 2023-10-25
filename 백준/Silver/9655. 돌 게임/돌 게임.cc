c9e72e0f0641ad8368a817061020fd2a88faebc7
ehgur062300 <79880998+ehgur062300@users.noreply.github.com>
2023-10-25 22:26:29 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.cc
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -4,20 +4,10 @@ using namespace std;
 
 int main(){
     int stones = 0;
-    string winner = "";
     cin >> stones;
     
-    for(int i=0; ; i++){
-        if(stones >= 3){ stones -= 3; }
-        else{ stones -= 1; }
-        
-        if(stones == 0){
-            if(i%2==0){ winner = "SK"; }
-            else{ winner = "CY"; }
-            break;
-        }
-    }
-    cout << winner;
+    if(stones%2 == 1){ cout << "SK"; }
+    else{ cout << "CY"; }
     
     return 0;
 }
