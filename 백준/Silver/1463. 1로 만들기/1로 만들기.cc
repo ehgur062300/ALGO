2a26e6eec626d34bd131fac8f861caf719ab52ef
ehgur062300 <79880998+ehgur062300@users.noreply.github.com>
2023-10-30 16:20:44 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5804 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<algorithm>
+
+#define IN cin.tie(NULL)
+#define OUT cout.tie(NULL)
+#define IOS ios::sync_with_stdio(false);
+
+using namespace std;
+
+int main(){
+    IN; OUT; IOS;
+    int dp[1000000];
+    int n;
+    cin >> n;
+
+    for(int i=2; i<=n; i++){
+        dp[i] = dp[i-1]+1;
+        
+        if(i%2==0){ dp[i]=min(dp[i],dp[i/2]+1); }
+        if(i%3==0){ dp[i]=min(dp[i], dp[i/3]+1); }
+    }
+
+    cout << dp[n];
+
+    return 0;
+}
