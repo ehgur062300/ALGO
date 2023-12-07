98f10f3ae2324a3c62f7047b996a214ad5d77184
Dohyuk Choi <79880998+ehgur062300@users.noreply.github.com>
2023-12-07 19:40:26 +0900

    [level unrated] Title: 모음 제거, Time: 0.11 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
@@ -2,13 +2,15 @@ class Solution {
     public String solution(String my_string) {
         String answer = "";
         
-        for(int i=0; i<my_string.length(); i++){
-            char c = my_string.charAt(i);
-            if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){
-                continue;
-            }
-            answer += c;
-        }
+        // for(int i=0; i<my_string.length(); i++){
+        //     char c = my_string.charAt(i);
+        //     if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){
+        //         continue;
+        //     }
+        //     answer += c;
+        // }
+        
+        answer = my_string.replaceAll("[aeiou]","");
         return answer;
     }
 }
