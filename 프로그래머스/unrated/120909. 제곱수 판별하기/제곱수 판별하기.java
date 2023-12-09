2256866209891856ed1b42748848dcf77582184c
Dohyuk Choi <79880998+ehgur062300@users.noreply.github.com>
2023-12-10 00:03:50 +0900

    [level unrated] Title: 제곱수 판별하기, Time: 0.04 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/unrated/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/unrated/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -2,10 +2,7 @@ import java.util.*;
 
 class Solution {
     public int solution(int n) {
-        double d_num = Math.sqrt(n);
-        double i_num = Math.floor(d_num);
-        
-        if(d_num == i_num){return 1;}
+        if(n % Math.sqrt(n) == 0){return 1;}
         return 2;
     }
 }
