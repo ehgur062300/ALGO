eec9760b21b485f073cd14cd14e81533a7d64459
Dohyuk Choi <79880998+ehgur062300@users.noreply.github.com>
2023-12-10 17:50:20 +0900

    [level unrated] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/unrated/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/unrated/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,8 +1,5 @@
 class Solution {
     public int solution(int n) {
-        int answer = n/7;
-        if(answer <= 1){return 1;}
-        if(n%7 > 0){return answer+1;}
-        return answer;
+        return (n+6)/7;
     }
 }
