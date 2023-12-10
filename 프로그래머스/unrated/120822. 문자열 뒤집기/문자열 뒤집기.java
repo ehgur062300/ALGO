3e472ece31a5f5badcdd7c74ba7f89168dc7010c
Dohyuk Choi <79880998+ehgur062300@users.noreply.github.com>
2023-12-10 16:16:09 +0900

    [level unrated] Title: 문자열 뒤집기, Time: 0.07 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/unrated/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -4,9 +4,8 @@ class Solution {
     public String solution(String my_string) {
         String answer = "";
         
-        for(int i=my_string.length()-1; i>=0; i--){
-            answer += my_string.charAt(i);
-        }
+        StringBuffer sb = new StringBuffer(my_string);
+        answer = sb.reverse().toString();
         return answer;
     }
 }
