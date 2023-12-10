7814c54555bad335d7568d7a3eb58565b6cb11a7
Dohyuk Choi <79880998+ehgur062300@users.noreply.github.com>
2023-12-10 16:59:38 +0900

    [level unrated] Title: 중복된 숫자 개수, Time: 0.02 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/unrated/120583. 중복된 숫자 개수/중복된 숫자 개수.java
+++ 프로그래머스/unrated/120583. 중복된 숫자 개수/중복된 숫자 개수.java
@@ -5,10 +5,19 @@ import java.util.stream.Collectors;
 class Solution {
     public int solution(int[] array, int n) {
         int answer = 0;
-        List<Integer> nums = 
-            Arrays.stream(array).boxed().collect(Collectors.toList());
         
-        answer = Collections.frequency(nums,n);
+        for(int num:array){
+            if(num == n){
+                answer++;
+            }
+        }
+        
+        
+        
+//         List<Integer> nums = 
+//             Arrays.stream(array).boxed().collect(Collectors.toList());
+        
+//         answer = Collections.frequency(nums,n);
         return answer;
     }
 }
