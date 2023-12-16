d9a03c58e9e7e7a89a97f5fd2a829c1f3b645db0
Dohyuk Choi <79880998+ehgur062300@users.noreply.github.com>
2023-12-17 00:44:42 +0900

    [level unrated] Title: 배열 자르기, Time: 1.56 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/unrated/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/unrated/120833. 배열 자르기/배열 자르기.java
@@ -1,10 +1,15 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
-        int len = num2-num1+1;
-        int[] answer = new int[len];
-        for(int i=0; i<len; i++){
-            answer[i] = numbers[i+num1];
-        }
+        int [] answer = {};
+        answer = Arrays.stream(numbers, num1, num2+1).toArray();
+        
+        // int len = num2-num1+1;
+        // int[] answer = new int[len];
+        // for(int i=0; i<len; i++){
+        //     answer[i] = numbers[i+num1];
+        // }
         return answer;
     }
 }
