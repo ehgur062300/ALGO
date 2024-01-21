f775bced6919ce584e6154befc66c773c327610b
Dohyuk Choi <79880998+ehgur062300@users.noreply.github.com>
2024-01-21 18:09:30 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 8.29 ms, Memory: 6.27 MB -BaekjoonHub

---
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<string> solution(string myString) {
+    vector<string> answer;
+    
+    string temp = "";
+    for(char c : myString){
+        if(c == 'x'){
+            if(temp == ""){continue;}
+            answer.push_back(temp);
+            temp = "";
+        }else{
+            temp += c;
+        }
+    }
+    if(temp != "") answer.push_back(temp);
+    sort(answer.begin(), answer.end());
+    return answer;
+}
