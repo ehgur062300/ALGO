ecad5666da3077d8947afa1aabd9a7f5a2e13014
Dohyuk Choi <79880998+ehgur062300@users.noreply.github.com>
2024-03-02 17:15:40 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
@@ -1,35 +1,48 @@
 #include <string>
 #include <vector>
-#include <unordered_map>
+// #include <unordered_map>
+#include <regex>
+
 using namespace std;
 
 int solution(string s) {
-    string answer = "";
-    unordered_map<string,char> m;
+    string answer = s;
+    // unordered_map<string,char> m;
     
-    m["zero"] = '0';
-    m["one"] = '1';
-    m["two"] = '2';
-    m["three"] = '3';
-    m["four"] = '4';
-    m["five"] = '5';
-    m["six"] = '6';
-    m["seven"] = '7';
-    m["eight"] = '8';
-    m["nine"] = '9';
+//     m["zero"] = '0';
+//     m["one"] = '1';
+//     m["two"] = '2';
+//     m["three"] = '3';
+//     m["four"] = '4';
+//     m["five"] = '5';
+//     m["six"] = '6';
+//     m["seven"] = '7';
+//     m["eight"] = '8';
+//     m["nine"] = '9';
     
-    string temp = "";
-    for(char c : s){
-        if(int(c) > 47 && int(c) < 58){
-            answer += c;
-        } else {
-            temp += c;
-            if(m.find(temp) != m.end()){
-                answer += m[temp];
-                temp = "";
-            }
-        }
-    }
+//     string temp = "";
+//     for(char c : s){
+//         if(int(c) > 47 && int(c) < 58){
+//             answer += c;
+//         } else {
+//             temp += c;
+//             if(m.find(temp) != m.end()){
+//                 answer += m[temp];
+//                 temp = "";
+//             }
+//         }
+//     }
+    
+    answer = regex_replace(answer,regex("zero"),"0");
+    answer = regex_replace(answer,regex("one"),"1");
+    answer = regex_replace(answer,regex("two"),"2");
+    answer = regex_replace(answer,regex("three"),"3");
+    answer = regex_replace(answer,regex("four"),"4");
+    answer = regex_replace(answer,regex("five"),"5");
+    answer = regex_replace(answer,regex("six"),"6");
+    answer = regex_replace(answer,regex("seven"),"7");
+    answer = regex_replace(answer,regex("eight"),"8");
+    answer = regex_replace(answer,regex("nine"),"9");
     
     return stoi(answer);
 }
