32496e0a69c13b446df04e369c3302dad28d0349
Dohyuk Choi <79880998+ehgur062300@users.noreply.github.com>
2024-03-27 00:08:10 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.cc
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -1,21 +1,10 @@
 #include <iostream>
 
 using namespace std;
-
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
+    int n,t=0;
+    while(cin>>n) { t += (n*n); }
+    cout << t%10;
     
-    int total = 0;
-    for (int i = 0; i < 5; i++) {
-        int num;
-        cin >> num;
-        
-        total += (num*num);
-    }
-    cout << total%10;
-    
-
     return 0;
 }
