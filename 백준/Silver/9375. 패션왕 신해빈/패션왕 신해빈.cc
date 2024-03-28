4d85758715a60309a492e3dd75481768c3819791
Dohyuk Choi <79880998+ehgur062300@users.noreply.github.com>
2024-03-28 17:33:55 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    for(int i=0; i<t; i++){
+        map<string,int> m;
+
+        int cnt, total=1;
+        cin >> cnt;
+
+        if(!cnt){
+            cout<<0<<"\n";
+            continue;
+        }
+        for(int j=0; j<cnt; j++){
+            string str1, str2;
+            cin >> str1;
+            cin >> str2;
+            m[str2]++;
+        }
+
+//        if(m.size() == 1 || m.empty()){
+//            cout << total;
+//            continue;
+//        }
+//
+//        int prev = m.begin()->second;
+//        int cur;
+//        map<string,int>::iterator it;
+//        for(it=++m.begin(); it!=m.end(); it++){
+//            cur = it->second;
+//            total += (cur*prev);
+//            prev = it->second;
+//        }
+
+        for(auto & v : m) {
+            total *= (v.second+1);
+        }
+        cout << --total << "\n";
+    }
+
+    return 0;
+}
