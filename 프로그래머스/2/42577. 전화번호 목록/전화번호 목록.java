e554c61968375e662b7106213c65347d248a59c0
Dohyuk Choi <79880998+ehgur062300@users.noreply.github.com>
2024-04-05 10:01:17 +0900

    [level 2] Title: 전화번호 목록, Time: 267.06 ms, Memory: 96.3 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -5,8 +5,7 @@ class Solution {
         
         Arrays.sort(phone_book);
         for(int i=1; i<phone_book.length; i++){
-            if(phone_book[i-1].length() > phone_book[i].length()){ continue; }
-            if(phone_book[i].substring(0,phone_book[i-1].length()).equals(phone_book[i-1])){
+            if(phone_book[i].startsWith(phone_book[i-1])){
                 return false;
             }
         }
