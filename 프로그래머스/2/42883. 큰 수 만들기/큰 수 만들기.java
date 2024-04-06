dfa96a670e09c642ccd1f7d860dadaaff8d2ce10
Dohyuk Choi <79880998+ehgur062300@users.noreply.github.com>
2024-04-06 14:08:39 +0900

    [level 2] Title: 큰 수 만들기, Time: 9.13 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,6 +1,21 @@
+import java.util.*;
+
 class Solution {
     public String solution(String number, int k) {
         String answer = "";
+        
+        int len = number.length()-k;
+        for(int i=0,idx = -1; i<len; i++){
+            char max = '0';
+            for(int j=idx+1; j<=k+i; j++){
+                if(max < number.charAt(j)){
+                    max = number.charAt(j);
+                    idx = j;
+                    if(max == '9'){ break; }
+                }
+            }
+            answer += max;
+        }
         return answer;
     }
 }
