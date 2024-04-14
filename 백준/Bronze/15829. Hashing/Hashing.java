f3f99aec589c272c3dccbca05fa6ee4783b1efb9
Dohyuk Choi <79880998+ehgur062300@users.noreply.github.com>
2024-04-15 08:58:14 +0900

    [Bronze II] Title: Hashing, Time: 120 ms, Memory: 14200 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        long[] arr = new long[t];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String s = st.nextToken();
+
+        arr[0] = 1;
+        for(int i=1; i<t; i++){ arr[i] = arr[i-1]*31; }
+
+        long sum = 0;
+        for(int i=0; i<s.length(); i++){
+            sum += (s.charAt(i)-96)*arr[i];
+        }
+        System.out.print(sum);
+
+    }
+}
