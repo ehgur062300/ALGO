2f606a943ff2d5100a1c33783e833a6ca99c8a8d
Dohyuk Choi <79880998+ehgur062300@users.noreply.github.com>
2024-07-24 21:45:28 +0900

    [Gold IV] Title: 문자열 폭발, Time: 404 ms, Memory: 25400 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -1,52 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
 public class Main {
     private static final String FRULA = "FRULA";
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         String str = br.readLine();
         String trigger = br.readLine();
-
         int triggerLength = trigger.length();
-        Stack<Character> stack = getCharacters(str, triggerLength, trigger);
-
-        if (stack.isEmpty()) {
-            System.out.print(FRULA);
-        } else {
-            StringBuilder sb = new StringBuilder();
-            for (char c : stack) {
-                sb.append(c);
-            }
-            System.out.print(sb);
-        }
-    }
-
-    private static Stack<Character> getCharacters(String str, int triggerLength, String trigger) {
-        Stack<Character> stack = new Stack<>();
-
+        
+        StringBuilder sb = new StringBuilder();
+        
         for (char c : str.toCharArray()) {
-            stack.push(c);
-            if (stack.size() >= triggerLength) {
+            sb.append(c);
+            if (sb.length() >= triggerLength) {
                 boolean match = true;
                 for (int i = 0; i < triggerLength; i++) {
-                    if (stack.get(stack.size() - triggerLength + i) != trigger.charAt(i)) {
+                    if (sb.charAt(sb.length() - triggerLength + i) != trigger.charAt(i)) {
                         match = false;
                         break;
                     }
                 }
                 if (match) {
-                    for (int i = 0; i < triggerLength; i++) {
-                        stack.pop();
-                    }
+                    sb.delete(sb.length() - triggerLength, sb.length());
                 }
             }
         }
-        return stack;
+        
+        System.out.print(sb.length() == 0 ? FRULA : sb.toString());
     }
 }
