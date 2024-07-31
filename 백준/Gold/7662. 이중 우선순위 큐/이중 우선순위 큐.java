23cade35abf42e37a96bb5822f0b70318e4a1e7a
Dohyuk Choi <79880998+ehgur062300@users.noreply.github.com>
2024-07-31 16:10:13 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2384 ms, Memory: 441960 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.StringTokenizer;
 import java.util.TreeMap;
 
 public class Main {
@@ -15,10 +16,14 @@ public class Main {
         while (T-- > 0) {
             nums = new TreeMap<>();
             int K = Integer.parseInt(br.readLine());
-
+            
             for (int i = 0; i < K; i++) {
-                StringBuilder commend = new StringBuilder(br.readLine());
-                applyCommend(commend);
+                StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+                
+                String operation = st.nextToken();
+                int num = Integer.parseInt(st.nextToken());
+                
+                applyCommend(operation,num);
             }
 
             if(nums.isEmpty())
@@ -30,17 +35,15 @@ public class Main {
         bw.flush();
     }
 
-    private static void applyCommend(StringBuilder cmd){
-        char operation = cmd.charAt(0);
-        char checkMinus = cmd.charAt(2);
-        int num = Integer.parseInt(String.valueOf(cmd.replace(0,2,"")));
-
-        if(operation == 'I'){
+    private static void applyCommend(String operation, int num){
+        
+        if(isInsert(operation)){
             nums.put(num,(nums.getOrDefault(num,0)+1));
 
         } else if(!nums.isEmpty()){
-            if(checkMinus == '-'){
+            if(isMinus(num)){
                 int key = nums.firstKey();
+                
                 if(getCount(key) > 1)
                     nums.put(key,nums.get(key)-1);
                 else
@@ -49,6 +52,7 @@ public class Main {
 
             else {
                 int key = nums.lastKey();
+                
                 if(getCount(key) > 1)
                     nums.put(key,nums.get(key)-1);
                 else
@@ -56,7 +60,15 @@ public class Main {
             }
         }
     }
-
+    
+    private static boolean isInsert(String operation){
+        return operation.equals("I");
+    }
+    
+    private static boolean isMinus(int num){
+        return num < 0;
+    }
+    
     private static int getCount(int key){
         return nums.get(key);
     }
