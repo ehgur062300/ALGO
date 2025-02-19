07e33eb9b7d7001b07fa9e5bd67e1f38d188e38f
Dohyuk Choi <79880998+ehgur062300@users.noreply.github.com>
2025-02-19 18:21:27 +0900

    [Silver III] Title: Four Squares, Time: 132 ms, Memory: 14688 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.java
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -1,35 +1,23 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
- 
+import java.io.IOException;
+
 public class Main {
- 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
- 
-        int[] dp = new int[N + 1];
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
         dp[1] = 1;
- 
-        int min = 0;
-        for (int i = 2; i <= N; i++) {
-            min = Integer.MAX_VALUE;
-            
-            // i에서 i보다 작은 제곱수에서 뺀 dp[i - j * j] 중
-            // 최소를 택한다.
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = i; // 최악의 경우 (1의 제곱수로만 표현)
             for (int j = 1; j * j <= i; j++) {
-                int temp = i - j * j;
-                min = Math.min(min, dp[temp]);
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
             }
- 
-            dp[i] = min + 1; // 그리고 1을 더해준다.
         }
- 
-        bw.write(dp[N] + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
+
+        System.out.println(dp[n]);
     }
 }
