dc442e7c84fc3e4655ea657c326914215cb48cb7
Dohyuk Choi <79880998+ehgur062300@users.noreply.github.com>
2025-03-09 17:45:02 +0900

    [Bronze IV] Title: 선린인터넷고등학교 교가, Time: 112 ms, Memory: 15360 KB -BaekjoonHub

---
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        String input = br.readLine();
+
+        System.out.print(input.substring(N-5));
+    }
+}
